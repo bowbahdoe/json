5e581f8a69f20a96a310e1ef77c07fed221b08ba
Ethan McCue <ethan@mccue.dev>
2023-07-26 18:36:46 -0400

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,7 @@ import org.jspecify.annotations.NullMarked;
 @SuppressWarnings("module")
 @NullMarked
 module dev.mccue.json {
-    requires static transitive org.jspecify;
+    requires static org.jspecify;
 
     exports dev.mccue.json;
     exports dev.mccue.json.stream;
